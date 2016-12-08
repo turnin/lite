2df4dd75230d1404dc12f211afd64496c3e30654
turnin <zhangjianping61@camelotchina.com>
2016-12-08 09:55:08 +0800

    修改了watch命令

--- lite/commands.go
+++ lite/commands.go
@@ -1,30 +1,59 @@
 package lite
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
-
 	"os/signal"
 	"syscall"
-
-	"fmt"
-
-	"io/ioutil"
+	"text/template"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/urfave/cli"
 	"github.com/xyproto/otto"
+	"path/filepath"
 )
+
+var adm = `
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+		{{range . }}
+			{{. -}}
+		{{end}}
+})();
+
+`
+
+var sdm = `
+  templates["%s"] = template(%s);
+`
+var tmpl *template.Template
+
+var precompileMap map[string]string
 
 var vm *otto.Otto
 
-func init() {
+func _init() {
 	vm = otto.New()
 	handlebarsJS, _ := Asset("handlebars.js")
 	_, err := vm.Run(handlebarsJS)
 	if err != nil {
 		log.Fatal(err)
 	}
+	precompileMap = make(map[string]string)
+	tmpl, _ = template.New("tmpl").Parse(adm)
+
+	fmt.Println("w init")
+
+	for _, path := range conf.Templates.Paths {
+		filepath.Walk(path, Walkfu)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	// vm.Set("Handlebars", vm.Get("Handlebars"))
 }
 
@@ -32,6 +61,7 @@ func init() {
 func WatchAction(c *cli.Context) error {
 
 	// beforeWatch()
+	_init()
 
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -45,21 +75,39 @@ func WatchAction(c *cli.Context) error {
 		for {
 			select {
 			case event := <-watcher.Events:
+			  _, name := filepath.Split(event.Name)
+				ext := filepath.Ext(name)
+				name = name[0:len(name)-len(ext)]
+
 				if event.Op&fsnotify.Write == fsnotify.Write {
-					fmt.Println(Emojitize(":coffee: modified file:" + event.Name))
-					precompile(event.Name)
-					// log.Println("modified file:", event.Name)
+					begin := time.Now()
+					content, _ := ioutil.ReadFile(event.Name)
+					precompile(name, content)
+
+					duration := time.Since(begin)
+					fmt.Printf("%s  in %.2fs\n", Emojitize(":coffee: compile file:" + event.Name),  duration.Seconds())
+
 				}
 				if event.Op&fsnotify.Remove == fsnotify.Remove {
-					fmt.Println(Emojitize(":coffee: delete file:" + event.Name))
 
-					// log.Println("delete file: ", event.Name)
+					begin := time.Now()
+					delete(precompileMap, name)
+					write()
+
+					duration := time.Since(begin)
+					fmt.Printf("%s  in %.2f|s\n", Emojitize(":coffee: compile file:" + event.Name),  duration.Seconds())
+
 				}
 				if event.Op&fsnotify.Rename == fsnotify.Rename {
-					fmt.Println(Emojitize(":coffee: delete file:" + event.Name))
-					// log.Println("rename file: ", event.Name)
-				}
 
+					begin := time.Now()
+					delete(precompileMap, name)
+					write()
+
+					duration := time.Since(begin)
+					fmt.Printf("%s  in %.2fs\n", Emojitize(":coffee: compile file:" + event.Name),  duration.Seconds())
+
+				}
 			case _ = <-watcher.Errors:
 				// log.Println("error: ", err)
 			}
@@ -80,9 +128,8 @@ func WatchAction(c *cli.Context) error {
 
 }
 
-func precompile(tmplPath string) error {
+func precompile(name string, content []byte) error {
 
-	content, _ := ioutil.ReadFile(tmplPath)
 	vm.Set("tmpl", string(content))
 	jsTmpl, err := vm.Run("Handlebars.precompile(tmpl)")
 
@@ -90,7 +137,21 @@ func precompile(tmplPath string) error {
 		return err
 	}
 
-	fmt.Println(jsTmpl)
+	precompileMap[name] = fmt.Sprintf(sdm, name, jsTmpl)
+
+	write()
 
 	return nil
+}
+
+func write() {
+	file, err := os.OpenFile(conf.Templates.Precompile, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+	if err != nil {
+		log.Println(err)
+		file ,_ = os.Create(conf.Templates.Precompile)
+	}
+	err = tmpl.Execute(file, precompileMap)
+	if err != nil {
+		log.Println(err)
+	}
 }
